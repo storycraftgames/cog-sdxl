291ba29da83959c7943adb2f8fd76cdd022b1bdc
Darren Grant <darren@storycraft.gg>
2024-03-16 17:07:59 -0400

    even more reduced

--- predict.py
+++ predict.py
@@ -3,20 +3,7 @@ import torch
 
 class Predictor(BasePredictor):
     def setup(self):
-        """Load the model or other assets here."""
-        # No model to load for this example, setup is used to prepare environment if necessary
         pass
 
-    @cog.input("check_cuda", type=bool, default=True, help="Check CUDA availability and list GPUs.")
     def predict(self, check_cuda):
-        """Check CUDA availability and GPU details."""
-        results = {"cuda_available": torch.cuda.is_available(), "gpus": []}
-        if results["cuda_available"] and check_cuda:
-            for i in range(torch.cuda.device_count()):
-                gpu_info = {
-                    "device_name": torch.cuda.get_device_name(i),
-                    "memory_allocated": torch.cuda.memory_allocated(i),
-                    "memory_cached": torch.cuda.memory_reserved(i)
-                }
-                results["gpus"].append(gpu_info)
-        return results
+        return "is_available:"+torch.cuda.is_available()
